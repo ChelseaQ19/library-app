8092122e85999ca7950f8db1638706c1a451d863
ChelseaQ19 <20096536@mail.wit.ie>
2024-11-14 15:35:23 +0000

    Added a new model called BookLocation to manage associations between book and location

--- src/main/kotlin/controllers/BookLocationAPI.kt
+++ src/main/kotlin/controllers/BookLocationAPI.kt
@@ -1,4 +1,15 @@
-package ie.setu.controllers
+package controllers
 
-class BookLocationAPI {
+import models.BookLocation
+
+class GroupContactController {
+
+    private val groupContacts = mutableListOf<GroupContact>()
+
+    fun addContactToGroup(contactId: Int, groupId: Int) {
+        groupContacts.add(GroupContact(contactId, groupId))
+    }
+
+    fun listContactsInGroup(groupId: Int) = groupContacts.filter { it.groupId == groupId }
+
 }
