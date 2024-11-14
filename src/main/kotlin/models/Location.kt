9d8ea5ecdcb0765c3ad09893edb235929a240ec8
ChelseaQ19 <20096536@mail.wit.ie>
2024-11-14 15:28:31 +0000

    Updated Location.kt and Book.kt to include unique IDs for new controller class, BookLocationAPI.kt

--- src/main/kotlin/models/Location.kt
+++ src/main/kotlin/models/Location.kt
@@ -1,6 +1,7 @@
 package ie.setu.models
 
-data class Location(val locationAisle: Int,
+data class Location(val locationId: Int,
+                    val locationAisle: Int,
                     val locationShelf: Int,
                     val locationIndex: Int,
                     val isLocationArchived :Boolean) {
