4929354e75c389d0a91353cf570eb5601958cc19
ChelseaQ19 <20096536@mail.wit.ie>
2024-12-06 10:32:46 +0000

    Created the JSONSerializer test for the LocationAPITest

--- src/test/kotlin/controllers/LocationAPItest.kt
+++ src/test/kotlin/controllers/LocationAPItest.kt
@@ -3,6 +3,7 @@ package controllers
 import ie.setu.controllers.LocationAPI
 import ie.setu.models.Location
 import ie.setu.persistance.XMLSerializer
+import ie.setu.persistance.JSONSerializer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
@@ -92,15 +93,15 @@ class LocationAPITest {
 
         @Test
         fun `saving and loading an empty collection in XML doesn't crash app`() {
-            // Saving an empty locations.XML file.
+
             val storingLocations = LocationAPI(XMLSerializer(File("locations.xml")))
             storingLocations.store()
 
-            //Loading the empty locations.xml file into a new object
+
             val loadedLocations = LocationAPI(XMLSerializer(File("locations.xml")))
             loadedLocations.load()
 
-            //Comparing the source of the locations (storingLocations) with the XML loaded locations (loadedLocations)
+
             assertEquals(0, storingLocations.numberOfLocations())
             assertEquals(0, loadedLocations.numberOfLocations())
             assertEquals(storingLocations.numberOfLocations(), loadedLocations.numberOfLocations())
@@ -108,18 +109,18 @@ class LocationAPITest {
 
         @Test
         fun `saving and loading an loaded collection in XML doesn't loose data`() {
-            // Storing 3 notes to the locations.XML file.
+
             val storingLocations = LocationAPI(XMLSerializer(File("locations.xml")))
             storingLocations.add(location1!!)
             storingLocations.add(location4!!)
             storingLocations.add(location5!!)
             storingLocations.store()
 
-            //Loading locations.xml into a different collection
+
             val loadedLocations = LocationAPI(XMLSerializer(File("locations.xml")))
             loadedLocations.load()
 
-            //Comparing the source of the locations (storingLocations) with the XML loaded notes (loadedLocations)
+
             assertEquals(3, storingLocations.numberOfLocations())
             assertEquals(3, loadedLocations.numberOfLocations())
             assertEquals(storingLocations.numberOfLocations(), loadedLocations.numberOfLocations())
@@ -127,5 +128,43 @@ class LocationAPITest {
             assertEquals(storingLocations.findLocation(4), loadedLocations.findLocation(4))
             assertEquals(storingLocations.findLocation(5), loadedLocations.findLocation(5))
         }
+    }
+
+    @Test
+    fun `saving and loading an empty collection in JSON doesn't crash app`() {
+
+        val storingLocations = LocationAPI(JSONSerializer(File("locations.json")))
+        storingLocations.store()
+
+
+        val loadedLocations = LocationAPI(JSONSerializer(File("locations.json")))
+        loadedLocations.load()
+
+
+        assertEquals(0, storingLocations.numberOfLocations())
+        assertEquals(0, loadedLocations.numberOfLocations())
+        assertEquals(storingLocations.numberOfLocations(), loadedLocations.numberOfLocations())
+    }
+
+    @Test
+    fun `saving and loading an loaded collection in JSON doesn't loose data`() {
+
+        val storingLocations = LocationAPI(JSONSerializer(File("locations.json")))
+        storingLocations.add(location1!!)
+        storingLocations.add(location4!!)
+        storingLocations.add(location5!!)
+        storingLocations.store()
+
+
+        val loadedLocations = LocationAPI(JSONSerializer(File("locations.json")))
+        loadedLocations.load()
+
+
+        assertEquals(3, storingLocations.numberOfLocations())
+        assertEquals(3, loadedLocations.numberOfLocations())
+        assertEquals(storingLocations.numberOfLocations(), loadedLocations.numberOfLocations())
+        assertEquals(storingLocations.findLocation(1), loadedLocations.findLocation(1))
+        assertEquals(storingLocations.findLocation(4), loadedLocations.findLocation(4))
+        assertEquals(storingLocations.findLocation(5), loadedLocations.findLocation(5))
     }
 }
